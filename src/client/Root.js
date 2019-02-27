359402c5556adf7ec366f731fbcdfe5c7acc5f80
FlorisSchippers <FlorisSchippers@gmail.com>
2019-02-27 13:02:37 +0100

    Fixed repo for new local setup

--- src/client/Root.js
+++ src/client/Root.js
@@ -1,12 +1,10 @@
-import React, {Component} from 'react';
-import {Switch, withRouter, Route} from 'react-router-dom';
-import appVars from "../config/appVars";
-import App from "../components/App";
-import Leerdoel from "../components/Leerdoel";
-import Competentie from "../components/Competentie";
-import Project from "../components/Project";
-import Leerdoelwapper from '../glamorous/header/Leerdoellist.jsx';
-import LeerdoelButton from '../glamorous/header/Leerdoelelement.jsx';
+import React, { Component } from 'react';
+import { Switch, withRouter, Route } from 'react-router-dom';
+import appVars from '../config/appVars';
+import App from '../components/App';
+import Leerdoel from '../components/Leerdoel';
+import Competentie from '../components/Competentie';
+import Project from '../components/Project';
 
 // We need a Root component for React Hot Loading.
 class Root extends Component {
@@ -21,54 +19,43 @@ class Root extends Component {
   };
 
   componentDidMount() {
-    if(!this.state.done.length > 0) {
+    if (!this.state.done.length > 0) {
       this.GetWordpressData();
     }
   }
 
   GetWordpressData() {
-    this.state.todo.forEach(function (leerdoel) {
-      fetch(appVars.api.live + leerdoel)
-      // fetch(appVars.api.local + leerdoel)
+    this.state.todo.forEach((leerdoel) => {
+      fetch(appVars.api.local + leerdoel) // Change back to live on production
         .then(results => results.json())
-        .then(function (data) {
+        .then((data) => {
           this.setState({
             [leerdoel]: [...data],
             done: [...this.state.done, leerdoel],
           });
-        }.bind(this));
-      // let array = this.state.todo;
-      // let index = array.indexOf(leerdoel);
-      // array.splice(index, 1);
-      // this.setState({
-      // [leerdoel]: array
-      // });
-      // console.log('removed: ' + leerdoel);
-    }.bind(this))
-  };
+        });
+    });
+  }
 
   render() {
     return (
       <Switch>
-        <Route exact path='/' component={App}/>
-        <Route exact path='/introductie' component={App}/>
-        <Route exact path='/burst' component={App}/>
-        <Route exact path='/conclusie' component={App}/>
-        <Route exact path='/ontwikkelen' component={p => <Leerdoel {...p} data={this.state.ontwikkelen} leerdoel={'ontwikkelen'}/>}/>
-        <Route exact path='/onderzoeken' component={p => <Leerdoel {...p} data={this.state.onderzoeken} leerdoel={'onderzoeken'}/>}/>
-        <Route exact path='/ontwerpen' component={p => <Leerdoel {...p} data={this.state.ontwerpen} leerdoel={'ontwerpen'}/>}/>
-        <Route exact path='/ondernemen' component={p => <Leerdoel {...p} data={this.state.ondernemen} leerdoel={'ondernemen'}/>}/>
-        <Route exact path='/betrokken' component={p => <Leerdoel {...p} data={this.state.betrokken} leerdoel={'betrokken'}/>}/>
-        <Route exact path='/ontwikkelen/:competentie' component={p => <Competentie {...p} data={this.state.ontwikkelen} leerdoel={'ontwikkelen'}/>}/>
-        <Route exact path='/ontwikkelen/:competentie/:project' component={p => <Project {...p} data={this.state.ontwikkelen} leerdoel={'ontwikkelen'}/>}/>
-        <Route exact path='/onderzoeken/:competentie' component={p => <Competentie {...p} data={this.state.onderzoeken} leerdoel={'onderzoeken'}/>}/>
-        <Route exact path='/onderzoeken/:competentie/:project' component={p => <Project {...p} data={this.state.onderzoeken} leerdoel={'onderzoeken'}/>}/>
-        <Route exact path='/ontwerpen/:competentie' component={p => <Competentie {...p} data={this.state.ontwerpen} leerdoel={'ontwerpen'}/>}/>
-        <Route exact path='/ontwerpen/:competentie/:project' component={p => <Project {...p} data={this.state.ontwerpen} leerdoel={'ontwerpen'}/>}/>
-        <Route exact path='/ondernemen/:competentie' component={p => <Competentie {...p} data={this.state.ondernemen} leerdoel={'ondernemen'}/>}/>
-        <Route exact path='/ondernemen/:competentie/:project' component={p => <Project {...p} data={this.state.ondernemen} leerdoel={'ondernemen'}/>}/>
-        <Route exact path='/betrokken/:competentie' component={p => <Competentie {...p} data={this.state.betrokken} leerdoel={'betrokken'}/>}/>
-        <Route exact path='/betrokken/:competentie/:project' component={p => <Project {...p} data={this.state.betrokken} leerdoel={'betrokken'}/>}/>
+        <Route exact path="/" component={App} />
+        <Route exact path="/ontwikkelen" component={p => <Leerdoel {...p} data={this.state.ontwikkelen} leerdoel={'ontwikkelen'} />} />
+        <Route exact path="/onderzoeken" component={p => <Leerdoel {...p} data={this.state.onderzoeken} leerdoel={'onderzoeken'} />} />
+        <Route exact path="/ontwerpen" component={p => <Leerdoel {...p} data={this.state.ontwerpen} leerdoel={'ontwerpen'} />} />
+        <Route exact path="/ondernemen" component={p => <Leerdoel {...p} data={this.state.ondernemen} leerdoel={'ondernemen'} />} />
+        <Route exact path="/betrokken" component={p => <Leerdoel {...p} data={this.state.betrokken} leerdoel={'betrokken'} />} />
+        <Route exact path="/ontwikkelen/:competentie" component={p => <Competentie {...p} data={this.state.ontwikkelen} leerdoel={'ontwikkelen'} />} />
+        <Route exact path="/ontwikkelen/:competentie/:project" component={p => <Project {...p} data={this.state.ontwikkelen} leerdoel={'ontwikkelen'} />} />
+        <Route exact path="/onderzoeken/:competentie" component={p => <Competentie {...p} data={this.state.onderzoeken} leerdoel={'onderzoeken'} />} />
+        <Route exact path="/onderzoeken/:competentie/:project" component={p => <Project {...p} data={this.state.onderzoeken} leerdoel={'onderzoeken'} />} />
+        <Route exact path="/ontwerpen/:competentie" component={p => <Competentie {...p} data={this.state.ontwerpen} leerdoel={'ontwerpen'} />} />
+        <Route exact path="/ontwerpen/:competentie/:project" component={p => <Project {...p} data={this.state.ontwerpen} leerdoel={'ontwerpen'} />} />
+        <Route exact path="/ondernemen/:competentie" component={p => <Competentie {...p} data={this.state.ondernemen} leerdoel={'ondernemen'} />} />
+        <Route exact path="/ondernemen/:competentie/:project" component={p => <Project {...p} data={this.state.ondernemen} leerdoel={'ondernemen'} />} />
+        <Route exact path="/betrokken/:competentie" component={p => <Competentie {...p} data={this.state.betrokken} leerdoel={'betrokken'} />} />
+        <Route exact path="/betrokken/:competentie/:project" component={p => <Project {...p} data={this.state.betrokken} leerdoel={'betrokken'} />} />
       </Switch>
     );
   }
--- src/config/appVars.js
+++ src/config/appVars.js
@@ -1,22 +1,22 @@
 const appVars = {
-    url: `${window.location.protocol}//${window.location.host}`,
-    title: 'Stageverslag',
-    api: {
-        local: 'https://stageverslag.local/wp-json/wp/v2/',
-        live: 'https://wordpress.florisschippers.nl/index.php/wp-json/wp/v2/',
-    },
-    theme: {
+  url: `${window.location.protocol}//${window.location.host}`,
+  title: 'Stageverslag',
+  api: {
+    local: 'http://stageverslag-wordpress.test/wp-json/wp/v2/',
+    live: 'http://wordpress.florisschippers.nl/index.php/wp-json/wp/v2/',
+  },
+  theme: {
         // lighter: '#DDFAA4',
         // light: '#BCE270',
         // primary: '#7B9F35',
         // dark: '#425B10',
         // darker: '#101800',
-        lighter: '#FBE0E3',
-        light: '#F75C6B',
-        primary: '#E0091E',
-        dark: '#99000F',
-        darker: '#4D0007',
-    }
+    lighter: '#FBE0E3',
+    light: '#F75C6B',
+    primary: '#E0091E',
+    dark: '#99000F',
+    darker: '#4D0007',
+  },
 };
 
 export default appVars;
